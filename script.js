0dd3fbb33df96275f8ed3d041785c84577efd485
João Victor <minutosdojv@gmail.com>
2025-11-12 12:07:35 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const intro = document.getElementById('intro');
-  setTimeout(() => {
-    intro.style.transition = "opacity 1s ease";
-    intro.style.opacity = "0";
-    setTimeout(() => intro.remove(), 1000);
-  }, 3000);
-});
